e271c8b75c8cec577a06a66462c8c29efbe3d1e4
谢泽丰 <616973936@qq.com>
2019-07-24 15:28:54 +0800

    【feat】增加根据权限列表对经济分析二级栏目的数据筛选处理（已测试）

--- src/main/java/com/wuhan_data/app/service/AnalysisService.java
+++ src/main/java/com/wuhan_data/app/service/AnalysisService.java
@@ -7,8 +7,11 @@ import com.wuhan_data.pojo.AnalysisPlate;
 import com.wuhan_data.pojo.AnalysisTheme;
 
 public interface AnalysisService {
-	public ArrayList<Object> getAnalysisList(); // 经济分析一级栏目列表接口
-	public ArrayList<Object> getAnalysisSubList(int typeId); // 经济分析二级栏目列表接口
-	public List<Object> getAnalysisRoleList(List<Object> analysisList, ArrayList<String> roleList);
-	public List<AnalysisPlate> getAnalysisPlate(int themeId); // 查询经济分析栏目下所有版块
+	public ArrayList<Object> getAnalysisList(int userId); // 经济分析一级栏目列表接口
+
+	public ArrayList<Object> getAnalysisSubList(int typeId, int userId); // 经济分析二级栏目列表接口
+
+	public List<AnalysisTheme> getAnalysisRoleList(List<AnalysisTheme> subList, ArrayList<String> roleList);
+
+	public ArrayList<Object> getAnalysisPlate(int themeId); // 查询经济分析栏目下所有版块
 }
