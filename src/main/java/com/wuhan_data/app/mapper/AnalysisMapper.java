2cf2990923d54f6cb0ec65c80252409a90bf8aff
谢泽丰 <616973936@qq.com>
2019-07-27 14:54:49 +0800

    【style】删除无用语句

--- src/main/java/com/wuhan_data/app/mapper/AnalysisMapper.java
+++ src/main/java/com/wuhan_data/app/mapper/AnalysisMapper.java
@@ -13,12 +13,6 @@ public interface AnalysisMapper {
 
 	public List<AnalysisTheme> getAnalysisSubList(int typeId); // 查询经济分析二级栏目列表
 
-	public Map<String, Object> getAnalysisDesc1(String indexName);
-
-	public Map<String, Object> getAnalysisDesc2(String indexName);
-
-	public Map<String, Object> getAnalysisDesc3(String indexName);
-
 	public List<AnalysisPlate> getAnalysisPlate(int themeId); // 查询指定经济分析栏目下所有版块
 
 	public List<AnalysisIndi> getIndiByPid(int plateId); // 查询指定版块下的所有指标配置项
