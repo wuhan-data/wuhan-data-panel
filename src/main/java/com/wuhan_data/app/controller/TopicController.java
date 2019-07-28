7c60ebe5ec83918af688aa267e979933f58a7a17
尹娇柔 <776490610@qq.com>
2019-07-28 16:29:55 +0800

    【fix】专题3和专题4

--- src/main/java/com/wuhan_data/app/controller/TopicController.java
+++ src/main/java/com/wuhan_data/app/controller/TopicController.java
@@ -670,7 +670,188 @@ public class TopicController {
 				@ResponseBody
 			    public String topic4(HttpServletRequest request, 
 			            HttpServletResponse response) throws UnsupportedEncodingException{
+					Map<String,Object> map = new HashMap<String,Object>();//最外层map
+					map.put("errCode", "0");//错误码
+					map.put("errMsg", "success");//错误消息--成功
 					
-					return "";
+					Plate plate = new Plate(4, "固定资产投资"); //创建专题信息（Id，title）
+					
+					Map<String,Object> map1 = new HashMap<String,Object>();
+					map1.put("baseInfo", plate);//baseInfo为专题基本信息
+//第一部分-图表1		
+					List<indi_TF> list1_1 = topicService.getTopic4Value1_1();
+					List<String> dataX = new ArrayList<String>();//X轴数据（时间轴）
+					List<List<Float>> dataV = new ArrayList<List<Float>>();//指标值
+					List<Float> dataV_1 = new ArrayList<Float>();//指标值
+					for(int i=0;i<list1_1.size();i++) {
+						dataX.add(list1_1.get(i).getDate_code());//从指标列表获取时间数据存入 X轴列表
+						dataV_1.add(Float.parseFloat(list1_1.get(i).getIndi_value()));//从指标列表获取指标值 并存入dataV
+					}
+					dataV.add(dataV_1);
+					List<String> legendData1 = new ArrayList<String>();//图例列表（指标名称）
+					legendData1.add(list1_1.get(0).getIndi_name());//
+					TopicLineType lineType = new TopicLineType();
+					LineEntity lineEntity1 = lineType.getOption("1", "湖北省投资先行指数变化趋势", dataX, legendData1, dataV,0,100);
+
+//第2部分-文字1	
+					String cardText1 = topicService.getTopic4Card1_1();
+					CardEntity cardEntity1 = new CardEntity("2", "全省投资分析", "card", cardText1);
+//第3部分-图表2
+					List<String> showType2 = new ArrayList<String>();
+					List<indi_TF> list2_1 = topicService.getTopic4Value2_1();
+					List<indi_TF> list2_2 = topicService.getTopic4Value2_2();
+					showType2.add("bar");
+					showType2.add("line");
+					
+					List<String> dataX2 = new ArrayList<String>();
+					List<List<Float>> dataV2 = new ArrayList<List<Float>>();
+					List<Float> dataVV2_1 = new ArrayList<Float>();
+					List<Float> dataVV2_2 = new ArrayList<Float>();
+					for(int i=0;i<list2_1.size();i++) {
+						dataX2.add(list2_1.get(i).getDate_code());
+						dataVV2_1.add(Float.parseFloat(list2_1.get(i).getIndi_value()));	
+					}
+					for(int i=0;i<list2_2.size();i++) {
+						dataVV2_2.add(Float.parseFloat(list2_2.get(i).getIndi_value()));	
+					}
+				
+					dataV2.add(dataVV2_1);
+					dataV2.add(dataVV2_2);
+
+					
+					List<String> legendData2 = new ArrayList<String>();
+					legendData2.add(list2_1.get(0).getIndi_name()+"-累计值");
+					legendData2.add(list2_2.get(0).getIndi_name()+"-增速");
+
+					
+					TopicLineAndBarType topicLineAndBarType2 = new TopicLineAndBarType();
+					LineAndBarEntity lineAndBarEntity2 = topicLineAndBarType2.getOption("3", "湖北省固定资产投资变化趋势", dataX2, legendData2, dataV2, showType2);
+//第4部分-文字2
+					String cardText2 = topicService.getTopic4Card2_1();
+					CardEntity cardEntity2 = new CardEntity("4", "本年投资分析", "card", cardText2);
+					
+//第5部分-图表3
+					List<String> showType3 = new ArrayList<String>();
+					List<indi_TF> list3_1 = topicService.getTopic4Value3_1();
+					List<indi_TF> list3_2 = topicService.getTopic4Value3_2();
+					List<indi_TF> list3_3 = topicService.getTopic4Value3_3();
+					List<indi_TF> list3_4 = topicService.getTopic4Value3_4();
+					showType3.add("bar");
+					showType3.add("line");
+					showType3.add("bar");
+					showType3.add("line");
+					
+					List<String> dataX3 = new ArrayList<String>();
+					List<List<Float>> dataV3 = new ArrayList<List<Float>>();
+					List<Float> dataVV3_1 = new ArrayList<Float>();
+					List<Float> dataVV3_2 = new ArrayList<Float>();
+					List<Float> dataVV3_3 = new ArrayList<Float>();
+					List<Float> dataVV3_4 = new ArrayList<Float>();
+					for(int i=0;i<list3_1.size();i++) {
+						dataX3.add(list3_1.get(i).getDate_code());
+						dataVV3_1.add(Float.parseFloat(list3_1.get(i).getIndi_value()));	
+					}
+					for(int i=0;i<list3_2.size();i++) {
+						dataVV3_2.add(Float.parseFloat(list3_2.get(i).getIndi_value()));	
+					}
+					
+					for(int i=0;i<list3_3.size();i++) {
+						dataVV3_3.add(Float.parseFloat(list3_3.get(i).getIndi_value()));	
+					}
+					for(int i=0;i<list3_4.size();i++) {
+						dataVV3_4.add(Float.parseFloat(list3_4.get(i).getIndi_value()));	
+					}
+				
+					dataV3.add(dataVV3_1);
+					dataV3.add(dataVV3_2);
+					dataV3.add(dataVV3_3);
+					dataV3.add(dataVV3_4);
+
+					
+					List<String> legendData3 = new ArrayList<String>();
+					legendData3.add(list3_1.get(0).getIndi_name()+"-累计值");
+					legendData3.add(list3_2.get(0).getIndi_name()+"-增速");
+					legendData3.add(list3_3.get(0).getIndi_name()+"-累计值");
+					legendData3.add(list3_4.get(0).getIndi_name()+"-增速");
+
+					
+					TopicLineAndBarType topicLineAndBarType3 = new TopicLineAndBarType();
+					LineAndBarEntity lineAndBarEntity3 = topicLineAndBarType3.getOption("5", "湖北省项目个数变化趋势", dataX3, legendData3, dataV3, showType3);		
+
+//第6部分-文字3
+					String cardText3 = topicService.getTopic4Card3_1();
+					CardEntity cardEntity3 = new CardEntity("6", "投资效益分析", "card", cardText3);
+
+//第7部分-图表4
+					
+					List<indi_TF> list4_1 = topicService.getTopic4Value4_1();
+					List<String> dataX4 = new ArrayList<String>();//X轴数据（时间轴）
+					List<List<Float>> dataV4 = new ArrayList<List<Float>>();//指标值
+					List<Float> dataV4_1 = new ArrayList<Float>();//指标值
+					for(int i=0;i<list4_1.size();i++) {
+						dataX4.add(list4_1.get(i).getDate_code());//从指标列表获取时间数据存入 X轴列表
+						dataV4_1.add(Float.parseFloat(list4_1.get(i).getIndi_value()));//从指标列表获取指标值 并存入dataV
+					}
+					dataV4.add(dataV4_1);
+					List<String> legendData4 = new ArrayList<String>();//图例列表（指标名称）
+					legendData4.add(list4_1.get(0).getIndi_name());//
+					TopicLineType lineType4 = new TopicLineType();
+					LineEntity lineEntity4 = lineType4.getOption("7", "湖北省投资效益系数变化趋势", dataX4, legendData4, dataV4,0,100);
+
+//第8部分-文字4
+					String cardText4 = topicService.getTopic4Card4_1();
+					CardEntity cardEntity4 = new CardEntity("8", "投资环境", "card", cardText4);
+					
+//第9部分-图表5				
+					List<String> showType5 = new ArrayList<String>();
+					List<indi_TF> list5_1 = topicService.getTopic4Value51_1();
+					List<indi_TF> list5_2 = topicService.getTopic4Value51_2();
+					showType5.add("bar");
+					showType5.add("line");
+					
+					List<String> dataX5 = new ArrayList<String>();
+					List<List<Float>> dataV5 = new ArrayList<List<Float>>();
+					List<Float> dataVV5_1 = new ArrayList<Float>();
+					List<Float> dataVV5_2 = new ArrayList<Float>();
+					for(int i=0;i<list5_1.size();i++) {
+						dataX5.add(list5_1.get(i).getDate_code());
+						dataVV5_1.add(Float.parseFloat(list5_1.get(i).getIndi_value()));	
+					}
+					for(int i=0;i<list5_2.size();i++) {
+						dataVV5_2.add(Float.parseFloat(list5_2.get(i).getIndi_value()));	
+					}
+
+				
+					dataV5.add(dataVV5_1);
+					dataV5.add(dataVV5_2);
+
+
+					
+					List<String> legendData5 = new ArrayList<String>();
+					legendData5.add(list5_1.get(0).getIndi_name());
+					legendData5.add(list5_2.get(0).getIndi_name());
+
+					
+					TopicLineAndBarType topicLineAndBarType5 = new TopicLineAndBarType();
+					LineAndBarEntity lineAndBarEntity5 = topicLineAndBarType5.getOption("9", "湖北省非公经济发展变化趋势", dataX5, legendData5, dataV5, showType5);		
+
+					
+					List<Object> classInfoList = new ArrayList<Object>();
+					classInfoList.add(lineEntity1);//图1
+					classInfoList.add(cardEntity1);
+					classInfoList.add(lineAndBarEntity2);//图2
+					classInfoList.add(cardEntity2);
+					classInfoList.add(lineAndBarEntity3);//图3
+					classInfoList.add(cardEntity3);
+					classInfoList.add(lineEntity4);//图4
+					classInfoList.add(cardEntity4);
+					classInfoList.add(lineAndBarEntity5);//图5
+					
+					
+					
+					map1.put("classInfo", classInfoList);
+					map.put("data", map1);
+					String  param= JSON.toJSONString(map); 
+			        return param;
 				}
 }
