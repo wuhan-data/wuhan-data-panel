ab4555309f979cbd1933ac21113374b9792b8f34
谢泽丰 <616973936@qq.com>
2019-08-22 21:21:08 +0800

    【fix】优化饼图

--- src/main/java/com/wuhan_data/app/showType/PieType.java
+++ src/main/java/com/wuhan_data/app/showType/PieType.java
@@ -12,13 +12,16 @@ public class PieType {
 	String type = "pie";
 
 	// 参数：指标或版块id、指标或版块名称、
-	public PieEntity getOption(String id, String title, List<String> dataV, List<String> legend) {
+	public PieEntity getOption(String id, String title, List<String> dataV, List<String> legend,
+			List<String> showColor) {
 
 		PieOptionEntity pieOptionEntity = new PieOptionEntity();
 
 		// 构建grid
 		Map<String, Object> gridMap = new HashMap<String, Object>();
 		gridMap.put("containLabel", true);
+		gridMap.put("bottom", "60");
+		gridMap.put("height", "250");
 		pieOptionEntity.setGrid(gridMap);
 
 		// 构建toopTip
@@ -29,30 +32,69 @@ public class PieType {
 
 		// 构建legend
 		Map<String, Object> legendMap = new HashMap<String, Object>();
-		legendMap.put("top", "top");
+		legendMap.put("orient", "vertical");
+		legendMap.put("bottom", "330");
 		legendMap.put("data", legend);
+		// 计算legend高度
+		int legendHeight = (legend.size() > 5 ? 5 : legend.size()) * 35;
+		legendMap.put("height", String.valueOf(legendHeight));
+		if (legend.size() > 5) {
+			legendMap.put("type", "scroll");
+		}
 		pieOptionEntity.setLegend(legendMap);
 
+		// 构建调色盘
+		List<String> colorMap = new ArrayList<String>();
+		// 一期调色盘
+		colorMap.add("#f0855b");
+		colorMap.add("#96cdf6");
+		colorMap.add("#8ac583");
+		colorMap.add("#6d95e6");
+		colorMap.add("#738c36");
+		colorMap.add("#ee74b2");
+		// 默认调色盘
+		colorMap.add("#c23531");
+		colorMap.add("#2f4554");
+		colorMap.add("#61a0a8");
+		colorMap.add("#d48265");
+		colorMap.add("#91c7ae");
+		colorMap.add("#749f83");
+		colorMap.add("#ca8622");
+		colorMap.add("#bda29a");
+		colorMap.add("#6e7074");
+		colorMap.add("#546570");
+		colorMap.add("#c4ccd3");
+		pieOptionEntity.setColor(colorMap);
+
 		// 构建series
-		Map<String, Object> seriesListMap = new HashMap<String, Object>();
+		Map<String, Object> seriesList = new HashMap<String, Object>();
 		List<Map<String, Object>> listTotal = new ArrayList<Map<String, Object>>();
 		List<Map<String, Object>> listData = new ArrayList<Map<String, Object>>();
 		for (int i = 0; i < dataV.size(); i++) {
-			Map<String, Object> mapData = new HashMap<String, Object>();
-			mapData.put("value", dataV.get(i));
-			mapData.put("name", legend.get(i));
-			listData.add(mapData);
+			Map<String, Object> seriesListMap = new HashMap<String, Object>();
+			seriesListMap.put("value", dataV.get(i));
+			seriesListMap.put("name", legend.get(i));
+			// 配置特定的颜色参数
+			Map<String, Object> seriesItemStyleMap = new HashMap<String, Object>();
+			if (i < showColor.size()) {
+				if (showColor.get(i) != null && showColor.get(i) != "") {
+					System.out.println(i + "has showColor");
+					seriesItemStyleMap.put("color", showColor.get(i).toString());
+				}
+			}
+			seriesListMap.put("itemStyle", seriesItemStyleMap);
+			listData.add(seriesListMap);
 		}
-		seriesListMap.put("type", "pie");
-		seriesListMap.put("radius", "50%");
+		seriesList.put("type", "pie");
+		seriesList.put("radius", "50%");
 		List<String> center = new ArrayList<String>();
 		center.add("50%");
 		center.add("50%");
-		seriesListMap.put("center", center);
-		seriesListMap.put("data", listData);
-		listTotal.add(seriesListMap);
+		seriesList.put("center", center);
+		seriesList.put("data", listData);
+		listTotal.add(seriesList);
 		pieOptionEntity.setSeries(listTotal);
-		
+
 		PieEntity pieEntity = new PieEntity(id, title, pieOptionEntity);
 		return pieEntity;
 	}
