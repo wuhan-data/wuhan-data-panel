9fd84d265c694cdec25c9cc7f4e2dc9108c6b6dc
孙朝阳 <41576214+Snowfallingplum@users.noreply.github.com>
2019-09-17 10:23:36 +0800

    ThmList二级分级实体类

---
+++ src/main/java/com/wuhan_data/tools/ThemeList.java
@@ -0,0 +1,28 @@
+package com.wuhan_data.tools;
+
+import java.util.List;
+
+import com.wuhan_data.pojo.AnalysisTheme;;
+
+public class ThemeList {
+	private String level_one;
+	private List<AnalysisTheme> level_twoInOneList;
+	public String getLevel_one() {
+		return level_one;
+	}
+	public void setLevel_one(String level_one) {
+		this.level_one = level_one;
+	}
+	public List<AnalysisTheme> getLevel_twoInOneList() {
+		return level_twoInOneList;
+	}
+	public void setLevel_twoInOneList(List<AnalysisTheme> level_twoInOneList) {
+		this.level_twoInOneList = level_twoInOneList;
+	}
+	@Override
+	public String toString() {
+		return "MenuList [level_one=" + level_one + ", level_twoInOneList=" + level_twoInOneList + "]";
+	}
+	
+
+}
