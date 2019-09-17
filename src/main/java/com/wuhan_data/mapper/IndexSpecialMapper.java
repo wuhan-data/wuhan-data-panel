c7b2a2d06eca32a84890de3883816bc413d0a37f
孙朝阳 <41576214+Snowfallingplum@users.noreply.github.com>
2019-09-17 10:31:06 +0800

    专题的mapper

---
+++ src/main/java/com/wuhan_data/mapper/IndexSpecialMapper.java
@@ -0,0 +1,12 @@
+package com.wuhan_data.mapper;
+
+import java.util.List;
+
+import com.wuhan_data.pojo.IndexSpecial;
+import com.wuhan_data.tools.DataSource;
+
+public interface IndexSpecialMapper {
+	@DataSource(value="dataSource_mysql")
+    public List<IndexSpecial> list(); 
+
+}
