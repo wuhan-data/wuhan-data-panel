468489b22163710838982b6bd630270aa1b0dbbe
谢泽丰 <616973936@qq.com>
2019-09-20 16:51:38 +0800

    【fix】构建经济分析 一级栏目-分类标签-二级栏目的数据结构

---
+++ src/main/java/com/wuhan_data/pojo/AnalysisLabel.java
@@ -0,0 +1,29 @@
+package com.wuhan_data.pojo;
+
+public class AnalysisLabel {
+	Integer labelId; // 分类标签id
+	Integer typeId; // 一级栏目id
+	String labelName; // 分类标签名称
+	Integer labelWeight; // 分类标签权重
+	Integer isShow; // 是否展示 0-正常 1-不展示 9-权限限制
+
+	public Integer geLabelId() {
+		return labelId;
+	}
+	
+	public Integer getTypeId() {
+		return typeId;
+	}
+
+	public String getLabelName() {
+		return labelName;
+	}
+	
+	public Integer getLabelWeight() {
+		return labelWeight;
+	}
+	
+	public Integer getIsShow() {
+		return isShow;
+	}
+}
