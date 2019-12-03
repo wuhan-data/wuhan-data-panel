32927968a4037960a5a03ff511100aa12ad4bf5e
陈伟 <1970559491@qq.com>
2019-12-03 21:48:07 +0800

    【fix】 修改后台栏目管理之前的

--- src/main/java/com/wuhan_data/app/controller/IndiSearchAppController.java
+++ src/main/java/com/wuhan_data/app/controller/IndiSearchAppController.java
@@ -430,7 +430,7 @@ public class IndiSearchAppController {
 			source = requestObject.get("source").toString();
 
 			isArea = requestObject.get("isArea").toString();// 判断是地市级数据还是全国数据
-			area_name = requestObject.get("area_name").toString();
+			area_name = requestObject.get("sourceArea").toString();
 			lj = requestObject.get("path").toString();
 		} catch (Exception e) {
 			return this.apiReturn("-1", "参数获取异常", data);
@@ -1326,7 +1326,7 @@ public class IndiSearchAppController {
 				boolean hasFreqName = requestObject.containsKey("timeFreq");
 				boolean hasStartTime = requestObject.containsKey("startTime");
 				boolean hasEndTime = requestObject.containsKey("endTime");
-				boolean hasAreaName = requestObject.containsKey("areaName");
+				boolean hasAreaName = requestObject.containsKey("sourceArea");
 				boolean hasLj = requestObject.containsKey("path");
 				if (!hasIndexCode) {
 					return this.apiReturn("-1", "需要指定栏目id", data);
@@ -1354,7 +1354,7 @@ public class IndiSearchAppController {
 				freqCode = requestObject.get("timeFreq").toString();
 				startTime = requestObject.get("startTime").toString();
 				endTime = requestObject.get("endTime").toString();
-				area_name = requestObject.get("area_name").toString();
+				area_name = requestObject.get("sourceArea").toString();
 				lj = requestObject.get("path").toString();
 			} catch (Exception e) {
 				return this.apiReturn("-1", "参数获取异常", data);
